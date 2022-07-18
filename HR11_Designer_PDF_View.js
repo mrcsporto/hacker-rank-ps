13ee2f2ff3d280af83c04d357d25a4f60ff48139
mrcsporto <mrcsporto@gmail.com>
2022-07-17 21:50:45 -0300

    HR11 Designer PDF viewer

---
+++ HR11_Designer_PDF_View.js
@@ -0,0 +1,26 @@
+let a = [1,3,1]
+let b = []
+let word = 'abc'
+
+for (let j = 0; j < word.length; j++) {
+    b.push(word[j])
+}
+console.log(b)
+
+
+// for (let i = 0; i < a.length; i++) {
+//     na.push(i)
+// }
+
+// console.log(b)
+
+// for (let i = 0; i < na.length; i++) {
+//     for (let j = 0; j < nb.length; j++) {
+//         if (na[i] == nb[j]){
+//             console.log(a[i])
+//         }
+        
+//     }
+// }
+
+
